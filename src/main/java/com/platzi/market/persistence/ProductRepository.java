a421e4eb2f7a877ee97f20f55acb86b0bedb7ee8
CrisFRC <ing.cristian.roac@gmail.com>
2023-04-08 20:00:48 -0500

    finish persitense layout and starte mapper to domain layout

--- src/main/java/com/platzi/market/persistence/ProductRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductRepository.java
@@ -2,13 +2,36 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductCrudRepository;
 import com.platzi.market.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository // esta anotacion indica que esta es la clase que trabaja directamente con la base de datos
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
     public List<Product> getAll(){
         return (List<Product>) productCrudRepository.findAll();
     }
+
+    public List<Product> getByCategory(int idCategory){
+        return productCrudRepository.findByIdCategoryOrderByNameAsc(idCategory);
+    }
+    public Optional<List<Product>> getNonStock(int stockNumber, boolean state){
+        return productCrudRepository.findByStockNumberLessThanAndState(stockNumber,state);
+    }
+    public Optional<Product>  getById(int idProduct){ //dado por los repositorios de spring data
+        return productCrudRepository.findById(idProduct);
+    }
+
+    public Product save(Product product){ //dado por los repositorios de spring data
+        return  productCrudRepository.save(product);
+    }
+
+    public void delete(int idProduct){
+        productCrudRepository.deleteById(idProduct);
+    }
+
+
 }
--- src/main/java/com/platzi/market/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductCrudRepository.java
@@ -3,6 +3,11 @@ package com.platzi.market.persistence.crud;
 import com.platzi.market.persistence.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductCrudRepository extends CrudRepository<Product,Integer> {
 
+    List<Product> findByIdCategoryOrderByNameAsc(int idCategory);
+    Optional<List<Product>> findByStockNumberLessThanAndState(int stockNumber, boolean state);
 }
